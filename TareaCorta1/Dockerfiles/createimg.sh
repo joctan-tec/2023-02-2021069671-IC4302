ecbb17b300e9a434aa5b91b23c3fcda13389807b
JRicardoCardona <jcardonar@estudiantec.cr>
2023-09-08 02:54:58 -0600

    agregado api mariadb

--- TareaCorta1/Dockerfiles/createimg.sh
+++ TareaCorta1/Dockerfiles/createimg.sh
@@ -1,16 +1,28 @@
-docker login
+sudo docker login
+cd api-postgresql
+
+cd database-client
+sudo docker build -t jcardonar/postgresql-client .
+sudo docker images
+sudo docker push jcardonar/postgresql-client
+
+cd ..
+sudo docker build -t jcardonar/api-postgresql .
+sudo docker images
+sudo docker push jcardonar/api-postgresql
+
+cd ..
 cd api-mariadb
 
 cd database-client
-docker build -t edwardandcode/api-mariadb .
+docker build -t jcardonar/api-mariadb .
 docker images
-docker push edwardandcode/api-mariadb
+docker push jcardonar/api-mariadb
 
 cd ..
-docker build -t edwardandcode/api-mariadb .
+docker build -t jcardonar/api-mariadb .
 docker images
-docker push edwardandcode/api-mariadb
-
+docker push jcardonar/api-mariadb
 
 
 # cd Dockerfiles
